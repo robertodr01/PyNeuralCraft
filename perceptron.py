9045203c168314a1b4b657522f079a592e577c5d
Michele Mattiello <michele.mattiello2001@gmail.com>
2023-12-03 17:33:50 +0100

    fix: vector operations

--- perceptron.py
+++ perceptron.py
@@ -7,11 +7,17 @@ class ActivationFunction:
     def derivative(self, x: np.ndarray):
         pass
 
+class Linear(ActivationFunction):
 
+    def output(self, x: np.ndarray):
+        return x
+
+    def derivative(self, x: np.ndarray):
+        return [1] * x.size
 class Perceptron:
 
     act_func: ActivationFunction
-    weights: list
+    weights: np.ndarray
     n_inputs: int
     eta = int
 
@@ -21,28 +27,30 @@ class Perceptron:
         if(n_inputs < 1):
             raise ValueError("Number of inputs must be greater than 0")
         self.act_func = act_func
-        self.weights = []
+        self.weights = np.array([])
         self.n_inputs = n_inputs
         self.eta = learning_rate
-        for _ in range(n_inputs):
-            self.weights.append(np.random.uniform(low=0.1, high=0.5))
+        for _ in range(n_inputs + 1):
+            value = np.random.uniform(low=0.1, high=0.5)
+            self.weights = np.append(self.weights, value)
 
     def run(self, x: np.ndarray):
+        x = np.insert(x, 0, 1)
         if isinstance(x, np.ndarray) == False:
             raise ValueError("Output must be a numpy list")
-        if len(x) != len(self.weights):
+        if x.size != self.weights.size:
             raise ValueError("Number of inputs must be equal to number of weights")
         net: float = np.dot(x, self.weights)
         out = self.act_func.output(np.array([net]))[0]
         return (net, out)
     
-    def train(self, oracle: int, net: int, output: int) -> np.ndarray:
+    def train(self, oracle: int, net: int, output: int, input: np.ndarray) -> np.ndarray:
+        input = np.insert(input, 0, 1)
         if isinstance(oracle, (int, float)) == False:
             raise ValueError("Oracle must be a int or float")
         if isinstance(output, (int, float)) == False:
             raise ValueError("Output must be a int or float")
-        der = self.act_func.derivative(np.array([net]))[0]
+        der = self.act_func.derivative(np.array([net]))[0] * input
         delta = (oracle - output) * der
-        for i in range(len(self.weights)):
-            self.weights[i] += self.eta * np.dot(delta, output)
+        self.weights = self.weights + self.eta * np.dot(delta, output)
         return delta
--- perceptron_test.py
+++ perceptron_test.py
@@ -1,15 +1,15 @@
-from perceptron import Perceptron, ActivationFunction
+from perceptron import Perceptron, Linear
 import numpy as np
 from metrics import Metrics
 input = [
-    np.array([4, 6]),
-    np.array([6, 4]),
-    np.array([2, 2]),
-    np.array([0, 4]),
-    np.array([2, 0]),
-    np.array([0, 0]),
-    np.array([6, 2]),
-    np.array([4, 4]),
+    [4, 6],
+    [6, 4],
+    [2, 2],
+    [0, 4],
+    [2, 0],
+    [0, 0],
+    [6, 2],
+    [4, 4],
 ]
 oracle = [
     1,
@@ -24,19 +24,17 @@ oracle = [
 
 def test_execute():
     metrics = Metrics()
-    act_func = ActivationFunction()
-    p = Perceptron(2, act_func, learning_rate=0.01)
-    for i in range(2000):
+    act_func = Linear()
+    p = Perceptron(len(input[0]), act_func, learning_rate=0.001)
+    for i in range(1000):
         for i in range(0, len(input)):
-            net, output = p.run(input[i])
-            p.train(oracle[i], net, output)
+            net, output = p.run(np.array(input[i]))
+            p.train(oracle[i], net, output, np.array(input[i]))
 
     for i in range(0, len(input)):
-        _, out = p.run(input[i])
-        print(out, oracle[i])
-        metrics.compute_results(out, oracle[i])
-    print(f"Accuracy: {metrics.accuracy()}")
-
-        
+        _, out = p.run(np.array(input[i]))
+        print(f'case {i}: {round(out), oracle[i]}')
+        metrics.compute_results(round(out), oracle[i])
+    print(f"Accuracy: {metrics.accuracy()}")    
 
 test_execute()
