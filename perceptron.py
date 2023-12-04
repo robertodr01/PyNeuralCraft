692ff43dab18397a57c6d43cd4f026cfef312336
Michele Mattiello <michele.mattiello2001@gmail.com>
2023-12-05 00:09:30 +0100

    feat(layer): add module and tester

--- perceptron.py
+++ perceptron.py
@@ -18,10 +18,11 @@ class Perceptron:
 
     act_func: ActivationFunction
     weights: np.ndarray
+    bias: float
     n_inputs: int
     eta = int
 
-    def __init__(self, n_inputs: int, act_func: ActivationFunction, learning_rate: float):
+    def __init__(self, n_inputs: int, act_func: ActivationFunction, learning_rate=0.001):
         if(isinstance(act_func, ActivationFunction) == False):
             raise TypeError("Activation function must be an instance of ActivationFunction")
         if(n_inputs < 1):
@@ -30,27 +31,26 @@ class Perceptron:
         self.weights = np.array([])
         self.n_inputs = n_inputs
         self.eta = learning_rate
-        for _ in range(n_inputs + 1):
+        self.bias = np.random.uniform(low=0.1, high=0.5)
+        for _ in range(n_inputs):
             value = np.random.uniform(low=0.1, high=0.5)
             self.weights = np.append(self.weights, value)
 
     def run(self, x: np.ndarray):
-        x = np.insert(x, 0, 1)
         if isinstance(x, np.ndarray) == False:
             raise ValueError("Output must be a numpy list")
         if x.size != self.weights.size:
             raise ValueError("Number of inputs must be equal to number of weights")
-        net: float = np.dot(x, self.weights)
+        net: float = np.dot(x, self.weights) + self.bias*1
         out = self.act_func.output(np.array([net]))[0]
         return (net, out)
     
-    def train(self, oracle: int, net: int, output: int, input: np.ndarray) -> np.ndarray:
+    def train(self, error: int, net: int, output: int, input: np.ndarray) -> np.ndarray:
         input = np.insert(input, 0, 1)
-        if isinstance(oracle, (int, float)) == False:
-            raise ValueError("Oracle must be a int or float")
-        if isinstance(output, (int, float)) == False:
-            raise ValueError("Output must be a int or float")
+        weights = np.insert(self.weights, 0, self.bias)
         der = self.act_func.derivative(np.array([net]))[0] * input
-        delta = (oracle - output) * der
-        self.weights = self.weights + self.eta * np.dot(delta, output)
-        return delta
+        delta = error * der
+        weights = weights + self.eta * np.dot(delta, output)
+        self.weights = weights[1:]
+        self.bias = weights[0]
+        return (delta, self.weights)
