fix: backprop

layer.py
@@ -1,9 +1,8 @@
 import numpy as np
-from perceptron import Perceptron
 from activation_function import ActivationFunction
 
 class Layer:
-    perceptrons: []
+
     def __init__(self, n_perceptrons, act_func=None, n_inputs=None, learning_rate=0.001):
         if(n_perceptrons==None and n_inputs==None):
             raise ValueError("perceptrons or number of inputs must be greater than 0")
@@ -11,28 +10,32 @@ class Layer:
             raise ValueError("Number of inputs must be greater than 0")
         if(isinstance(act_func, ActivationFunction) == False):
             raise TypeError("Activation function must be an instance of ActivationFunction")
-        self.perceptrons = []
-        for _ in range(n_perceptrons):
-            self.perceptrons.append(Perceptron(n_inputs, act_func, learning_rate))
+        self.weights = np.random.rand(n_perceptrons, n_inputs)
+        self.biases = np.random.rand(n_perceptrons, 1)
+        self.act_func = act_func
+        self.eta = learning_rate
 
     def run(self, x: np.ndarray) -> tuple:
         outs = np.array([])
         nets = np.array([])
-        for i in range(len(self.perceptrons)):
-            (net, out) = self.perceptrons[i].run(x)
+        for i in range(len(self.weights)):
+            net = np.dot(self.weights[i], x) + self.biases[i][0]
+            out = self.act_func.output(np.array([net]))[0]
             outs = np.append(outs, out)
             nets = np.append(nets, net)
         return (nets, outs)
 
-    def train(self, errors: np.ndarray, net: np.ndarray, output: np.ndarray):
-        propagate_errors = np.array([0.0] * self.perceptrons[0].weights.size)
-        for i in range(len(self.perceptrons)):
-            propagate_errors += self.perceptrons[i].train(errors[i], net[i], output[i])
-            #print(propagate_errors)
+    def train(self, errors: np.ndarray, net: np.ndarray, output: np.ndarray, input):
+        propagate_errors = np.array([0.0] * len(self.weights[0]))
+        for i in range(len(self.weights)):
+            der = self.act_func.derivative(np.array([net[i]]))[0]
+            delta = errors[i] * der
+            self.weights[i] += self.eta * delta * output[i] * input
+            self.biases[i][0] += self.eta * delta * output[i]
+            propagate_errors += delta * self.weights[i]
         return propagate_errors
     
     def summary(self):
-        for perceptron in self.perceptrons:
+        for i in range(len(self.weights)):
             print(f"{10*'-'} Perceptron {10*'-'}")
-            perceptron.summary()
-            print()
+            print(self.weights[i])

mlp.py
@@ -34,15 +34,13 @@ class MLP:
                     outputs_for_layer.append(output)
                     inputs_for_layer.append(input_for_layer)
                     input_for_layer = output
-                #np.round(output, 2)
                 error = (np.array(oracle[j]) - output)
                 global_error += error
                 for k in range(len(self.layers) - 1, -1, -1):
-                    propagate_errors = self.layers[k].train(error, nets_for_layer[k], outputs_for_layer[k])
+                    propagate_errors = self.layers[k].train(error, nets_for_layer[k], outputs_for_layer[k], inputs_for_layer[k])
                     error = propagate_errors
             f.write(f"{global_error}\n")
             f.close()
-            # print(np.array(oracle[j]), output)
                  
     def summary(self):
         for layer in self.layers: