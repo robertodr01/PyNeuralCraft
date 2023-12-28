61d2c1b7234367757f5e330cca5913ada15fc9bc
Michele Mattiello <michele.mattiello2001@gmail.com>
2023-12-28 22:59:34 +0100

    feat: notebook

--- mlp_test.py
+++ mlp_test.py
@@ -3,26 +3,35 @@ from layer import Layer
 from mlp import MLP
 import numpy as np
 from metrics import Metrics
+import csv
+from sklearn import preprocessing
 
 # xor tests
-input = [[0, 0], [0, 1], [1, 0], [1, 1]]
-oracle = [[0], [1], [1], [0]]
+inputs = []
+oracles = []
+
+filename = "Iris.csv"
+n_oracle = 3
+
+print(inputs[0])
+#print(oracles)
 
 def test_execute():
     mlp = MLP()
     metrics = Metrics()
     act_func1 = ReLU()
     act_func2 = Sigmoid()
-    lr = 0.1
+    lr = 0.001
     f = open("logs.txt", "w")
     f.close()
-    mlp.add(Layer(5, act_func=act_func1, n_inputs=len(input[0]), learning_rate=lr))
-    mlp.add(Layer(len(oracle[0]), act_func=act_func2, n_inputs=5, learning_rate=lr))
+    mlp.add(Layer(9, act_func=act_func1, n_inputs=len(inputs[0]), learning_rate=lr))
+    #mlp.add(Layer(9, act_func=act_func1, n_inputs=8, learning_rate=lr))
+    mlp.add(Layer(len(oracles[0]), act_func=act_func2, n_inputs=9, learning_rate=lr))
     #mlp.summary()
     
-    mlp.fit(input, oracle, epochs=1000)
-    for i in range(len(input)):
-        out = mlp.run(input[i])
-        print(f'case {i}: {np.round(out, 2), oracle[i]}')
+    mlp.fit(inputs, oracles, epochs=1000)
+    for i in range(len(inputs)):
+        out = mlp.run(inputs[i])
+        print(f'case {i}: {np.round(out), oracles[i]}')
     #mlp.summary()
 test_execute()
